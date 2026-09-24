69dec86bc40f662619f000388463daf839e94038
MathiasPariona <mathias.pariona@utec.edu.pe>
2026-09-23 20:56:20 -0500

    Dependencies

---
+++ src/main/java/com/example/qsale/QsaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.qsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QsaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QsaleApplication.class, args);
+	}
+
+}
